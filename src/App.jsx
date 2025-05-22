51231d33823951a731b479c1f8f199a3f8c01cd4
Grashad <awtomat74m@gmail.com>
2025-05-22 22:06:55 +0200

    HTML restructured into components

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+function MainPage() {
+     return (<>   <main>
+        <section id="welcome-section">
+            <h1>Welcome to my page</h1>
+            <h2>My name is Grashad</h2>
+            <p class="web-dev">i'm a webdev</p>
+            <p class="web-dev font">Please enjoy your stay!</p>
+        </section>
+
+        <section id="end-section">
+            <h2 class="end-text">There’s not a lot to see yet, but you can still check it out</h2>
+            <p class="end-text">though there will be more to come!</p>
+            <a id="profile-link" href="https://github.com/Grashad" target="_blank"><i class="fab fa-github"></i></a>
+        </section>
+    </main></>
+     )
+}
+export default MainPage;
+
+
+
+
+
+
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,3 @@
+ <footer id="footer-1">
+        <p>Created by Grashad 2025</p>
+    </footer>
