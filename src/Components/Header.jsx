05e3414004404f112cf8cc731f9e76472c8a6312
Grashad <awtomat74m@gmail.com>
2025-05-22 18:52:54 +0200

    Starting Point of the Project No major changes

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+    
+function PortfolioHeader() {    
+    
+    <header id="navbar">
+        <ul>
+            <li><a href="#welcome-section">About</a></li>
+            <li><a href="#project-section">Work</a></li>
+            <li><a href="#end-section">Contact</a></li>
+        </ul>
+    </header>
+}
---
+++ src/Components/ProjectCard.jsx
@@ -0,0 +1,14 @@
+       
+       
+       
+       
+       
+ function ProjectCard() {      
+       <section id="project-section">
+            <a class="alink inside" href="#">
+                <img class="project-img" src="https://media.tenor.com/0EDznml5BDAAAAAj/cat-spinning.gif"
+                    loading="lazy" />
+                <h2 class="project-tile">Placeholder</h2>
+            </a>
+            </section>
+}
